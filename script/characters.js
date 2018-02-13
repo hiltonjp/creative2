0db5102624b2835f65fb181d54b9b4a3f94582d1
Jeff <jeffhilton.code@gmail.com>
2018-02-13 05:28:18 +0000

    blah

--- script/characters.js
+++ script/characters.js
@@ -20,6 +20,16 @@ $(document).ready(function(){
       $("li").on("click",function() {
         var id = this.id-1;
         $("#char_pic").attr("src",json[id].MainImageUrl);
+
+        /*Color Maniputlation*/
+	var color1 = json[id].ColorTheme;
+	var color2 = shadeColor(color1,0.6);
+	
+        $(".left").css("background-color",color1);
+        $("body").css("background-color",color2); 
+        $("#attributes_box").css("background-color",color1);
+        $("#attacks_box").css("background-color",color1);
+
         $("#character_name").text(json[id].DisplayName);
         var movesURL = json[id].Links[1].Href;
         var attrURL = json[id].Links[2].Href;
@@ -81,4 +91,9 @@ $(document).ready(function(){
     } //success function
   }); //ajax();
 
-}); //ready();
+}); //ready()
+
+function shadeColor(color, percent) {   
+    var f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
+    return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
+}

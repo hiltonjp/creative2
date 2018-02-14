be0e7350971c722a835d5867065b4a324699c0ed
Jeff Hilton <jeffhilton.code@gmail.com>
2018-02-13 18:35:09 -0700

    Finished some responsive design!

--- script/characters.js
+++ script/characters.js
@@ -9,26 +9,33 @@ $(document).ready(function(){
     success: function(json) {
       console.log(json);
       var structure = "<ul class='char_select'>";
+      var selection = "";
       json.forEach(function(character){
-        structure += "<li class='char_option' id='" + character.OwnerId + "'><button>" + character.DisplayName + "</button></li>";
+        structure += "<li class='char_option' id='" + character.OwnerId + "'><button class='char_button'>" + character.DisplayName + "</button></li>";
+        selection += "<option value='" + character.MainImageUrl + "' OwnerId='"+ character.OwnerId +"' theme='" + character.ColorTheme + "'>" + character.DisplayName + "</option>";
       });
       structure += "</ul>";
       $(".left").append(structure);
+      $("#fighter").append(selection);
 
+      $("#fighter").change(function() {
+        $("#tempChoice").remove();
 
-      //make a click handler for all characters in list;
-      $("li").on("click",function() {
-        var id = this.id-1;
-        $("#char_pic").attr("src",json[id].MainImageUrl);
+        var id = $("#fighter > option:selected").attr('OwnerId');
+        id = id -1;
+        var theme = $("#fighter > option:selected").attr('theme');
+        var adjusted = shadeColor(theme,0.3);
+        var adjusted2 = shadeColor(theme,0.6);
 
-        /*Color Maniputlation*/
-	var color1 = json[id].ColorTheme;
-	var color2 = shadeColor(color1,0.6);
-	
-        $(".left").css("background-color",color1);
-        $("body").css("background-color",color2); 
-        $("#attributes_box").css("background-color",color1);
-        $("#attacks_box").css("background-color",color1);
+        console.log(theme);
+        $("#char_pic").attr("src",$(this).val());
+
+        $(".left").css("background-color",theme);
+        $(".choice").css("background-color",theme);
+        $("#fighter").css("background-color",adjusted2);
+        $("body").css("background-color",adjusted2);
+        $("#attributes_box").css("background-color",theme);
+        $("#attacks_box").css("background-color",theme);
 
         $("#character_name").text(json[id].DisplayName);
         var movesURL = json[id].Links[1].Href;
@@ -40,9 +47,7 @@ $(document).ready(function(){
           success: function(movesJson) {
             console.log(movesJson);
 
-            structure = "<h2 id='attack_title'>Attacks</h2>";
-
-            structure += "<table id='attack_table'>";
+            structure = "<table id='attack_table'>";
               structure += "<tr>";
                 structure += "<th>Attack</th>";
                 structure += "<th>Base Damage</th>";
@@ -66,8 +71,77 @@ $(document).ready(function(){
           success: function(attrJson) {
             console.log(attrJson);
 
-            structure = "<h2 id='attribute_title'>Attributes</h2>";
-            structure += "<table id='attribute_table'>";
+            structure = "<table id='attribute_table'>";
+              structure += "<tr>";
+                structure += "<th>Attribute</th>";
+                structure += "<th>Value</th>";
+              structure += "</tr>";
+              attrJson.forEach(function(attr){
+                structure += "<tr>";
+                  structure += "<td>" + attr.Name + "</td>";
+                  structure += "<td>" + attr.Values[0].Value + "</td>";
+                structure += "</tr>";
+              });
+            structure += "</table>";
+            $("#attributes_box").empty();
+            $("#attributes_box").append(structure); //put attributes after attacks
+          }
+        });
+
+      });
+
+      //make a click handler for all characters in list;
+      $("li").on("click",function() {
+        $("#tempChoice").remove();
+        var id = this.id-1;
+        $("#char_pic").attr("src",json[id].MainImageUrl);
+
+        /*Color Maniputlation*/
+      	var color1 = json[id].ColorTheme;
+      	var color2 = shadeColor(color1,0.6);
+
+        $(".left").css("background-color",color1);
+        $(".choice").css("background-color",color1);
+        $("#fighter").css("background-color",color2);
+        $("body").css("background-color",color2);
+        $("#attributes_box").css("background-color",color1);
+        $("#attacks_box").css("background-color",color1);
+
+        $("#character_name").text(json[id].DisplayName);
+        var movesURL = json[id].Links[1].Href;
+        var attrURL = json[id].Links[2].Href;
+        ///*
+        $.ajax({
+          url: movesURL,
+          dataType: "json",
+          success: function(movesJson) {
+            console.log(movesJson);
+
+            structure = "<table id='attack_table'>";
+              structure += "<tr>";
+                structure += "<th>Attack</th>";
+                structure += "<th>Base Damage</th>";
+              structure += "</tr>";
+              movesJson.forEach(function(move) {
+                structure += "<tr>";
+                  structure += "<td>" + move.Name + "</td>";
+                  structure += "<td>" + move.BaseDamage + "</td>";
+                structure += "</td>";
+              });
+            structure += "</table>";
+
+            $("#attacks_box").empty();
+            $("#attacks_box").append(structure);
+          }
+        }); //set up moves list
+
+        $.ajax({
+          url: attrURL,
+          dataType: "json",
+          success: function(attrJson) {
+            console.log(attrJson);
+
+            structure = "<table id='attribute_table'>";
               structure += "<tr>";
                 structure += "<th>Attribute</th>";
                 structure += "<th>Value</th>";
@@ -93,7 +167,7 @@ $(document).ready(function(){
 
 }); //ready()
 
-function shadeColor(color, percent) {   
+function shadeColor(color, percent) {
     var f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
     return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
 }
---
+++ script/versus.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+  var charDataURL = "http://beta-api-kuroganehammer.azurewebsites.net/api/characters";
+  $.ajax({
+    url: charDataURL,
+    dataType: "json",
+    success: function(json) {
+      console.log(json);
+      var structure = "";
+      json.forEach(function(character){
+        structure += "<option value='" + character.MainImageUrl + "' theme='"+ character.ColorTheme +"'>" + character.DisplayName + "</option>";
+      });
+
+      $("#fighter1").append(structure);
+      $("#fighter2").append(structure);
+
+      $("#fighter1").change(function() {
+        var theme = $("#fighter1 > option:selected").attr('theme');
+        var adjusted = shadeColor(theme,0.3);
+        var adjusted2 = shadeColor(theme,0.6);
+
+        console.log(theme);
+        $("img[name=left_character]").attr("src",$(this).val());
+        $("#left_character").css("background-color",adjusted);
+        $("#choice1").css("background-color",adjusted);
+        $("#fighter1").css("background-color",adjusted2);
+      }).trigger("change");
+
+      $("#fighter2").change(function() {
+        var theme = $("#fighter2 > option:selected").attr('theme');
+        var adjusted = shadeColor(theme,0.3);
+        var adjusted2 = shadeColor(theme,0.6);
+
+        console.log(theme);
+        $("img[name=right_character]").attr("src",$(this).val());
+        $("#right_character").css("background-color",adjusted);
+        $("#choice2").css("background-color",adjusted);
+        $("#fighter2").css("background-color",adjusted2);
+      }).trigger("change");
+    }
+  });
+
+  $("#fight_btn").on("click",function(e){
+    e.preventDefault();
+    var char1 = $("#fighter1 > option:selected").text();
+    var char2 = $("#fighter2 > option:selected").text();
+
+    if (char1 !== "Choose a Character" && char2 !== "Choose a Character") {
+      var winner = Math.random();
+      console.log(winner);
+      if (winner < 0.5) {
+        $("#battleWinner").text("WINNER: " + char1 + "!");
+      } else {
+        $("#battleWinner").text("WINNER: " + char2 + "!");
+      }
+    } else {
+      $("#battleWinner").text("Please choose your characters!");
+    }
+
+  });
+});
+
+function shadeColor(color, percent) {
+    var f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
+    return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
+}
